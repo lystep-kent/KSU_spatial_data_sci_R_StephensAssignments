09485d9e675a4040b2a28fc892c43ee66ef86938
Patrick Bitterman <patrick.bitterman@gmail.com>
2025-01-27 16:36:20 -0500

    week 3

--- src/week02_scratch.R
+++ src/week02_scratch.R
@@ -1,6 +1,8 @@
 # week 2 scratch pad
 
 library(tidyverse)
+library(terra)
+library(sf)
 
 mydf <- read_csv("./data/oh_counties_DP2020.csv")
 glimpse(mydf)
@@ -41,3 +43,38 @@ mydf2 %>% ggplot(., aes(x = sizeCategory, y = medianage)) +
        y = "Median Age", 
        title = "I made a boxplot",
        subtitle = "...it's handy for comparing groups")
+
+
+
+rastmiss <- terra::rast("./data/ts_2016.1007_1013.L4.LCHMP3.CIcyano.MAXIMUM_7day.tif")
+
+
+p.counties <- "./data/CBW/County_Boundaries.shp"
+p.stations <- "./data/CBW/Non-Tidal_Water_Quality_Monitoring_Stations_in_the_Chesapeake_Bay.shp"
+
+d.counties <- terra::vect(p.counties)
+d.stations <-terra::vect(p.stations)
+
+glimpse(d.counties)
+glimpse(d.stations)
+
+
+del.counties <- d.counties %>% dplyr::filter(STATEFP10 == 10)
+del.counties <- d.counties %>% terra::subset(d.counties$STATEFP10 == "10")
+
+d.counties <- sf::read_sf(p.counties)
+d.stations <- sf::read_sf(p.stations)
+
+d.counties %>% sf::st_crs() == d.stations %>% sf::st_crs()
+
+
+de.stations <- sf::st_intersection(d.stations, del.counties) # might take a bit
+glimpse(de.stations)
+plot(de.stations)
+
+
+option_1 <- sf::st_intersection(d.stations, del.counties)
+option_2 <- sf::st_intersection(del.counties, d.stations)
+
+
+
